b5019b21a23794b1244ed1859044bcd3cbcb22a5
VOLT_SYNAPSE <vadim.khristenko@gmail.com>
2024-01-06 15:35:41 +0300

    alpha-0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FOXFORD_API',
-    version='0.1.0',
+    version='0.1.0.1',
     description='FOXFORD Student API\nБиблиотека для Работы с Бекэндом Платформы FOXFORD',
     long_description="""
 FOXFORD Student API
