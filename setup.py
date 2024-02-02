aa9acca07ce4618fbc338e8de22978a06b049409
VOLT_DIAMOND <vadim.khristenko@gmail.com>
2024-02-02 16:16:37 +0300

    Update setup.py
    Signed-off-by: VOLT_DIAMOND <vadim.khristenko@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FOXFORD_API',
-    version='0.1.4a4',
+    version='0.1.4a5',
     description='FOXFORD Student API\nБиблиотека для Работы с Бекэндом Платформы FOXFORD',
     long_description="""
 FOXFORD Student API
